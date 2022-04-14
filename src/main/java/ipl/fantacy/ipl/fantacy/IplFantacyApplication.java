a0390c4daa47e855fb5ac95343ab98a347b78284
sushree barsarani <sushreebarsarani132@gmail.com>
2022-04-14 11:38:52 +0530

    Provide comment

---
+++ src/main/java/ipl/fantacy/ipl/fantacy/IplFantacyApplication.java
@@ -0,0 +1,13 @@
+package ipl.fantacy.ipl.fantacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplFantacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplFantacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ipl/fantacy/ipl/fantacy/IplFantacyApplicationTests.java
@@ -0,0 +1,13 @@
+package ipl.fantacy.ipl.fantacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplFantacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
